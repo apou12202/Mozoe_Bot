a9231f5dbe4ae27f78d949a3fe83028bb29dd312
WeiChi <apou12202@gmail.com>
2020-08-04 20:15:29 +0800

    mod bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,13 @@ from discord.ext import commands
 from discord.utils import get
 from discord import Embed, Emoji
 import json
+import random
 
 with open('setting.json', 'r', encoding='utf-8') as jfile:
     jdata = json.load(jfile)
+
+with open('emoji.json', 'r', encoding='utf-8') as jemoji:
+    jmoji = json.load(jemoji)
 
 bot = commands.Bot(command_prefix='')  #set command title
 
@@ -17,9 +21,9 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)}(ms)')
 
-#@bot.command()
-#async def mozoe(ctx):
-
-
+@bot.command()
+async def 貓肉(ctx):
+    rnum = random.randint(0, len(jmoji['ming'])-1)
+    await ctx.send(jmoji['ming'][rnum])
 #bot.run(token) start
 bot.run(jdata['TOKEN'])
