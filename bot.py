4e240a54c22cc2c879066c6b08f00a5cb308be4d
WeiChi <apou12202@gmail.com>
2020-08-06 21:27:45 +0800

    mod bot.py

--- bot.py
+++ bot.py
@@ -17,13 +17,23 @@ bot = commands.Bot(command_prefix='')  #set command title
 async def on_ready():
     print(">>Bot is online<<")
 
+@bot.event
+async def on_message(message):
+    # if message.content.find("5"):
+    #     await message.channel.send("Hi")
+    if message.content.startswith("那貓肉"):
+        await message.channel.send(jmoji['ming'][random.randint(0, len(jmoji['ming'])-1)])
+        print("on_message test")
+    await bot.process_commands(message)   # https://stackoverflow.com/questions/49331096/why-does-on-message-stop-commands-from-working
+
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)}(ms)')
 
 @bot.command()
 async def 貓肉(ctx):
-    rnum = random.randint(0, len(jmoji['ming'])-1)
-    await ctx.send(jmoji['ming'][rnum])
+    # rnum = random.randint(0, len(jmoji['ming'])-1)
+    await ctx.send(jmoji['ming'][random.randint(0, len(jmoji['ming'])-1)])
+    print("貓肉emoji test")
 #bot.run(token) start
 bot.run(jdata['TOKEN'])
